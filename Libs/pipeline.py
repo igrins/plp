5c95bed0425298c5c778c2fbd7b3e87d3ac5d7cd
Jae-Joon Lee <lee.j.joon@gmail.com>
2014-06-05 17:07:50 +0900

    add pipeline.py and refactor readechellogram

---
+++ Libs/pipeline.py
@@ -0,0 +1,20 @@
+
+if 1:
+
+
+
+
+    def readechellogram(item_list):
+
+        imgs = []
+        hdrs = []
+        for item in item_list:
+            img, hdr = readfits(item)
+            #img_min = array(img).min()
+            #print 'item', item
+            #if img_min <= 0:
+            #    img -= img_min
+
+            imgs.append(img)
+            hdrs.append(hdr)
+        return imgs,hdrs
