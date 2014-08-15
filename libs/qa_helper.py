8f69e0ffbe0122ef09dbce1cee85cf0114eae55b
Jae-Joon Lee <lee.j.joon@gmail.com>
2014-08-15 17:29:42 +0900

    add fig_to_json

--- libs/qa_helper.py
+++ libs/qa_helper.py
@@ -10,3 +10,15 @@ def figlist_to_pngs(rootname, figlist, postfixes=None):
         fig.savefig("%s_%s.png" % (rootname, postfix))
         #fig2.savefig("align_zemax_%s_fig2_fit.png" % postfix)
         #fig3.savefig("align_zemax_%s_fig3_hist_dlambda.png" % postfix)
+
+def figlist_to_json(rootname, figlist, postfixes=None):
+    from matplotlib.backends.backend_agg import FigureCanvasAgg
+    from itertools import izip, count
+    from mpld3 import save_json
+
+    if postfixes is None:
+        postfixes = ("fig%02d" % i for i in count(1))
+
+    for postfix, fig in izip(postfixes, figlist):
+        FigureCanvasAgg(fig)
+        save_json(fig, "%s_%s.json" % (rootname, postfix))
