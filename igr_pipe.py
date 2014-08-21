cbc64b192b93479353facfc0e7474d56d2716ae3
Jae-Joon Lee <lee.j.joon@gmail.com>
2014-08-22 03:06:55 +0900

    add missing igr_pipe.py

---
+++ igr_pipe.py
@@ -0,0 +1,24 @@
+import argh
+
+import recipes.recipe_flat
+import recipes.recipe_thar
+import recipes.recipe_wvlsol_sky
+from recipes.recipe_distort_sky import distortion_sky
+from recipes.recipe_extract import (a0v_ab, stellar_ab,
+                                    extended_ab, extended_onoff)
+
+recipe_list = [recipes.recipe_flat.flat,
+               recipes.recipe_thar.thar,
+               recipes.recipe_wvlsol_sky.wvlsol_sky,
+               distortion_sky,
+               a0v_ab,
+               stellar_ab,
+               extended_ab,
+               extended_onoff
+               ]
+
+parser = argh.ArghParser()
+parser.add_commands(recipe_list)
+
+if __name__ == '__main__':
+    argh.dispatch(parser)
