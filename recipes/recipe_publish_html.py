6f96310886264fdbd8361d9b98a4d300104949a2
Jae-Joon Lee <lee.j.joon@gmail.com>
2014-11-20 11:34:13 +0900

    publish-html support a0v-onoff & stellar-onoff

--- recipes/recipe_publish_html.py
+++ recipes/recipe_publish_html.py
@@ -18,6 +18,8 @@ def make_html(utdate, dirname, config_file="recipe.config"):
         if r in ["A0V_AB", "STELLAR_AB",
                  "EXTENDED_AB",
                  "EXTENDED_ONOFF",
+                 "A0V_ONOFF",
+                 "STELLAR_ONOFF",
                  ]:
             s = dict(zip(["name", "obj", "grp1", "grp2", "exptime", "recipe", "obsids", "frametypes"],
                          desc))
