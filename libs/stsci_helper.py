71878e0d018aeea6b5a1120f5c75cbc94ed84845
Jae-Joon Lee <lee.j.joon@gmail.com>
2015-03-25 19:34:34 +0900

    make stsci.image as a requirement

--- libs/stsci_helper.py
+++ libs/stsci_helper.py
@@ -1,12 +1,3 @@
 import numpy as np
 
-try:
-    from stsci.image import median as stsci_median
-except ImportError:
-    stsci_median = None
-
-if stsci_median is None:
-    def stsci_median(arrs, badmasks=None):
-        dd1 = np.ma.array(arrs, mask=badmasks)
-        ddm = np.ma.median(dd1, axis=0)
-        return ddm
+from stsci.image import median as stsci_median
