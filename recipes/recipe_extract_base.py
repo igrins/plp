072481493ac8518731db78b6c1356dde73d2db86
Jae-Joon Lee <lee.j.joon@gmail.com>
2015-08-09 14:40:40 +0900

    refactor lazyprop as a separate file

--- recipes/recipe_extract_base.py
+++ recipes/recipe_extract_base.py
@@ -2,14 +2,7 @@ import numpy as np
 import astropy.io.fits as pyfits
 import scipy.ndimage as ni
 
-def lazyprop(fn):
-    attr_name = '_lazy_' + fn.__name__
-    @property
-    def _lazyprop(self):
-        if not hasattr(self, attr_name):
-            setattr(self, attr_name, fn(self))
-        return getattr(self, attr_name)
-    return _lazyprop
+from libs.lazyprop import lazyprop
 
 import os
 from libs.products import ProductDB, PipelineStorage
