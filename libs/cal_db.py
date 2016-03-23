igrins/plp

commit 40592b824f2e90e1c758fa84b5e85e32b3552f73

initial attempt for updating the wavelength solution framework

libs/cal_db.py
@@ -208,11 +208,14 @@ class CalDB(object):
 
         band, master_obsid = self._get_band_masterobsid(basename)
 
-        db_name, item_desc = self.RESOURCE_DICT[resource_type]
-        basename = self.db_query_basename(db_name, band, master_obsid)
+        try:
+            db_name, item_desc = self.RESOURCE_DICT.get(resource_type,
+                                                        resource_type)
+            db_name, item_desc = resource_type
+        except ValueError as e:
+            raise e  # it would be good if we can modify the message
 
         return basename, item_desc
-
 
     def load_resource_for(self, basename, resource_type):
         """

recipes/nd_poly.py
@@ -1,0 +1,156 @@
+import numpy as np
+
+from itertools import product
+from collections import namedtuple
+
+
+def my_namedtuple(n, names):
+
+    _kls = namedtuple(n, names)
+
+    class kls(_kls):
+        def __new__(cls, args):
+            return super(kls, cls).__new__(cls, *args)
+
+        def __getitem__(self, k):
+            if k in self._fields:
+                return getattr(self, k)
+            else:
+                return super(kls, self).__getitem__(k)
+
+    kls.__name__ = n
+
+    return kls
+
+
+class NdPoly(object):
+    def _setup(self, orders, orderT, names):
+        po_list = [orderT(_) for _ in product(*(range(o+1) for o in orders))]
+
+        self.orders = orderT(orders)
+        self.names = names
+        self.orderT = orderT
+        self.po_list = po_list
+
+    def __init__(self, orders):
+        names = range(len(orders))
+        orderT = tuple
+
+        self._setup(orders, orderT, names)
+
+    def multiply(self, vv, coeffs):
+        v = 0.
+        for po, p in zip(self.po_list, coeffs):
+            v1 = np.multiply.reduce([pow(vv[k], po[k])
+                                     for k in self.names])
+            v += p*v1
+
+        return v
+
+    def get_array(self, vv):
+        v_list = []
+        for po in self.po_list:
+            v1 = np.multiply.reduce([pow(vv[k], po[k])
+                                     for k in self.names])
+            v_list.append(v1)
+
+        return v_list
+
+    def _get_frozen_p(self, k_survived):
+        p = NdPoly([self.orders[_k] for _k in k_survived])
+        return p
+
+    def freeze(self, k, v, coeffs):
+
+        k_survived = tuple(_k for _k in self.names if _k != k)
+        p = self._get_frozen_p(k_survived)
+        # p = NdPoly([self.orders[_k] for _k in k_survived], k_survived)
+
+        poo = dict((_k, []) for _k in p.po_list)
+
+        for c1, po in zip(coeffs, self.po_list):
+            _ = (o for _k, o in zip(self.names, po) if _k != k)
+            nk = p.orderT(_)
+            oo = po[k]
+            poo[nk].append(c1 * pow(v, oo))
+
+        sol1 = [np.sum(poo[po]) for po in p.po_list]
+
+        return p, sol1
+
+    def to_pandas(self, **kwargs):
+        """
+        convert to pandas dataframe.
+        """
+
+        import pandas as pd
+        index = pd.MultiIndex.from_tuples(self.po_list, names=self.names)
+        df = pd.DataFrame(index=index, data=kwargs)
+
+        return df
+
+    @staticmethod
+    def from_pandas(df):
+        # df.index.values
+        # df.index.names
+        # df = coeffs
+
+        orders = df.index.values.max(axis=0)
+        p = NdPolyNamed(orders, df.index.names)
+
+        coeffs = df.loc[p.po_list].values.reshape([-1,])
+
+        return p, coeffs
+
+        # coeffs = pd.read_json("coeffs.json", orient="split")
+
+
+class NdPolyNamed(NdPoly):
+    def __init__(self, orders, names):
+        orderT = my_namedtuple("order_" + "_".join(names), names)
+
+        self._setup(orders, orderT, names)
+
+    def _get_frozen_p(self, k_survived):
+        p = NdPolyNamed([self.orders[_k] for _k in k_survived], k_survived)
+        return p
+
+
+#         T = namedtuple("order_"+"_".join(_), _)
+
+# fix_k = "order"
+# fix_value = 80
+
+# _ = tuple(k for k in names if k != fix_k)
+# T = namedtuple("order_"+"_".join(_), _)
+
+# po_list = [T(*_) for _ in product([0, 1, 2], [0, 1, 2])]
+# poo = dict((k, []) for k in po_list)
+
+# for sol0, po in zip(sol, po_list0):
+#     _ = (p for k, p in zip(names, po) if k != fix_k)
+#     k = T(*_)
+#     #print po, T(*_)
+#     oo = getattr(po, fix_k)
+#     poo[k].append(sol0 * pow(fix_value, oo))
+
+# sol1 = [np.sum(poo[po]) for po in po_list]
+
+
+#def test():
+if 1:
+    p0 = NdPoly([2, 2, 2])
+    params0 = np.arange(1, 28)
+    x0 = p0.multiply([2, 3, 5], params0)
+    p1, params1 = p0.freeze(0, 2, params0)
+    x1 = p1.multiply([3, 5], params1)
+    assert x0 == x1
+    p2, params2 = p0.freeze(1, 3, params0)
+    x2 = p2.multiply([2, 5], params2)
+    assert x0 == x2
+
+if 1:
+    names = ["i1", "i2", "i3"]
+    p0 = NdPolyNamed([2, 2, 2], names)
+    params0 = np.arange(1, 28)
+    x0 = p0.multiply(dict(zip(names, [2, 3, 5])), params0)

recipes/recipe_wvlsol_sky2.py
@@ -1,0 +1,397 @@
+# This is to use new framework. Let's use this to measure flexure
+# between emission spectra (e.g., sky, UNe, etc.)
+
+#import os
+import numpy as np
+
+#from libs.process_flat import FlatOff, FlatOn
+
+#from libs.path_info import IGRINSPath
+#import astropy.io.fits as pyfits
+
+#from libs.products import PipelineProducts
+#from libs.apertures import Apertures
+
+#from libs.products import PipelineProducts
+
+#from libs.recipe_base import RecipeBase
+
+
+def save_wavelength_sol(helper, band, obsids,
+                        orders_w_solutions, wvl_sol, p):
+
+    caldb = helper.get_caldb()
+
+    d = dict(orders=orders_w_solutions,
+             wvl_sol=wvl_sol)
+    master_obsid = obsids[0]
+    caldb.store_dict((band, master_obsid), "SKY_WVLSOL_JSON", d)
+
+    if 1:  # save as WAT fits header
+        xx = np.arange(0, 2048)
+        xx_plus1 = np.arange(1, 2048+1)
+
+        from astropy.modeling import models, fitting
+
+        # We convert 2d chebyshev solution to a seriese of 1d
+        # chebyshev.  For now, use naive (and inefficient)
+        # approach of refitting the solution with 1d. Should be
+        # reimplemented.
+
+        p1d_list = []
+        for o in orders_w_solutions:
+            oo = np.empty_like(xx)
+            oo.fill(o)
+            wvl = p(xx, oo) / o * 1.e4  # um to angstrom
+
+            p_init1d = models.Chebyshev1D(domain=[1, 2048],
+                                          degree=p.x_degree)
+            fit_p1d = fitting.LinearLSQFitter()
+            p1d = fit_p1d(p_init1d, xx_plus1, wvl)
+            p1d_list.append(p1d)
+
+    from libs.iraf_helper import get_wat_spec, default_header_str
+    wat_list = get_wat_spec(orders_w_solutions, p1d_list)
+
+    # cards = [pyfits.Card.fromstring(l.strip()) \
+    #          for l in open("echell_2dspec.header")]
+    import astropy.io.fits as pyfits
+    cards = [pyfits.Card.fromstring(l.strip())
+             for l in default_header_str]
+
+    wat = "wtype=multispec " + " ".join(wat_list)
+    char_per_line = 68
+    num_line, remainder = divmod(len(wat), char_per_line)
+    for i in range(num_line):
+        k = "WAT2_%03d" % (i+1,)
+        v = wat[char_per_line*i:char_per_line*(i+1)]
+        #print k, v
+        c = pyfits.Card(k, v)
+        cards.append(c)
+    if remainder > 0:
+        i = num_line
+        k = "WAT2_%03d" % (i+1,)
+        v = wat[char_per_line*i:]
+        #print k, v
+        c = pyfits.Card(k, v)
+        cards.append(c)
+
+    if 1:
+        # save fits with empty header
+
+        header = pyfits.Header(cards)
+        hdu = pyfits.PrimaryHDU(header=header,
+                                data=np.array([]).reshape((0,0)))
+
+        caldb.store_image((band, master_obsid),
+                          "SKY_WVLSOL_FITS", np.array(wvl_sol),
+                          hdu=hdu)
+
+    # if 0:
+    #     # plot all spectra
+    #     for w, s in zip(wvl_sol, s_list):
+    #         plot(w, s)
+
+
+def save_qa(helper, band, obsids,
+            orders_w_solutions,
+            reidentified_lines_map, p, m):
+    # filter out the line indices not well fit by the surface
+
+    keys = reidentified_lines_map.keys()
+    di_list = [len(reidentified_lines_map[k_][0]) for k_ in keys]
+
+    endi_list = np.add.accumulate(di_list)
+
+    filter_mask = [m[endi-di:endi] for di, endi in zip(di_list, endi_list)]
+
+    reidentified_lines_ = [reidentified_lines_map[k_] for k_ in keys]
+
+    _ = [(v_[0][mm], v_[1][mm]) for v_, mm
+         in zip(reidentified_lines_, filter_mask)]
+
+    reidentified_lines_map_filtered = dict(zip(orders_w_solutions, _))
+
+    if 1:
+        from matplotlib.figure import Figure
+        from libs.ecfit import get_ordered_line_data, check_fit
+
+        xl, yl, zl = get_ordered_line_data(reidentified_lines_map)
+
+        fig1 = Figure(figsize=(12, 7))
+        check_fit(fig1, xl, yl, zl, p,
+                  orders_w_solutions,
+                  reidentified_lines_map)
+        fig1.tight_layout()
+
+        fig2 = Figure(figsize=(12, 7))
+        check_fit(fig2, xl[m], yl[m], zl[m], p,
+                  orders_w_solutions,
+                  reidentified_lines_map_filtered)
+        fig2.tight_layout()
+
+    from libs.qa_helper import figlist_to_pngs
+    igr_path = helper.igr_path
+    sky_basename = helper.get_basename(band, obsids[0])
+    sky_figs = igr_path.get_section_filename_base("QA_PATH",
+                                                  "oh_fit2d",
+                                                  "oh_fit2d_"+sky_basename)
+    figlist_to_pngs(sky_figs, [fig1, fig2])
+
+
+class WavelengthFitter(object):
+    pass
+
+
+class SkyFitter(WavelengthFitter):
+    def get_refdata(self, band):
+        from libs.master_calib import load_sky_ref_data
+
+        if band not in self._refdata:
+            sky_refdata = load_sky_ref_data(self.config, band)
+            self._refdata[band] = sky_refdata
+
+        return self._refdata[band]
+
+    def __init__(self, config, refdate):
+        self.config = config
+        self.refdate = refdate
+        self._refdata = {}
+
+    def fit_individual_orders(self, band,
+                              orders_w_solutions, wvl_solutions, s_list):
+
+        sky_ref_data = self.get_refdata(band)
+        ohline_indices = sky_ref_data["ohline_indices"]
+        ohlines_db = sky_ref_data["ohlines_db"]
+
+        from libs.reidentify_ohlines import fit_ohlines
+
+        _ = fit_ohlines(ohlines_db, ohline_indices,
+                        orders_w_solutions,
+                        wvl_solutions, s_list)
+        ref_pixel_list, reidentified_lines = _
+
+        return ref_pixel_list, reidentified_lines
+
+    def update_K(self, reidentified_lines_map,
+                 orders_w_solutions,
+                 wvl_solutions, s_list):
+        # import libs.master_calib as master_calib
+        # fn = "hitran_bootstrap_K_%s.json" % self.refdate
+        # bootstrap_name = master_calib.get_master_calib_abspath(fn)
+        # import json
+        # bootstrap = json.load(open(bootstrap_name))
+
+        from libs.master_calib import load_ref_data
+        bootstrap = load_ref_data(self.config, band="K",
+                                  kind="HITRAN_BOOTSTRAP_K")
+
+
+        import libs.hitran as hitran
+        r, ref_pixel_list = hitran.reidentify(orders_w_solutions,
+                                              wvl_solutions, s_list,
+                                              bootstrap)
+        # json_name = "hitran_reidentified_K_%s.json" % igrins_log.date
+        # r = json.load(open(json_name))
+        for i, s in r.items():
+            ss = reidentified_lines_map[int(i)]
+            ss0 = np.concatenate([ss[0], s["pixel"]])
+            ss1 = np.concatenate([ss[1], s["wavelength"]])
+            reidentified_lines_map[int(i)] = (ss0, ss1)
+
+        return reidentified_lines_map
+
+    def convert2wvlsol(self, p, orders_w_solutions):
+
+        # derive wavelengths.
+        xx = np.arange(2048)
+        wvl_sol = []
+        for o in orders_w_solutions:
+            oo = np.empty_like(xx)
+            oo.fill(o)
+            wvl = p(xx, oo) / o
+            wvl_sol.append(list(wvl))
+
+        return wvl_sol
+
+    def fit(self, band, orders_w_solutions, wvl_solutions, s_list):
+        ref_pixel_list, reidentified_lines = \
+                            self.fit_individual_orders(band,
+                                                       orders_w_solutions,
+                                                       wvl_solutions, s_list)
+
+        from libs.ecfit import get_ordered_line_data, fit_2dspec
+
+        reidentified_lines_map = dict(zip(orders_w_solutions,
+                                          reidentified_lines))
+
+        if band == "K":
+            self.update_K(reidentified_lines_map,
+                          orders_w_solutions,
+                          wvl_solutions, s_list)
+
+        xl, yl, zl = get_ordered_line_data(reidentified_lines_map)
+        # xl : pixel
+        # yl : order
+        # zl : wvl * order
+
+        x_domain = [0, 2047]
+        y_domain = [orders_w_solutions[0]-2, orders_w_solutions[-1]+2]
+        x_degree, y_degree = 4, 3
+        #x_degree, y_degree = 3, 2
+        p, m = fit_2dspec(xl, yl, zl, x_degree=x_degree, y_degree=y_degree,
+                          x_domain=x_domain, y_domain=y_domain)
+
+        wvl_sol = self.convert2wvlsol(p, orders_w_solutions)
+
+        return orders_w_solutions, wvl_sol, reidentified_lines_map, p, m
+
+
+def fit_oh_spectra(refdate, band,
+                   orders_w_solutions,
+                   wvl_solutions, s_list):
+
+    fitter = SkyFitter(refdate)
+    _ = fitter.fit(band, orders_w_solutions, wvl_solutions, s_list)
+
+    orders_w_solutions, wvl_sol, reidentified_lines_map, p, m = _
+
+    return orders_w_solutions, wvl_sol, reidentified_lines_map, p, m
+
+
+
+# 20151003 : Below is an attemp to modularize the recipes, which has
+# not finished. Initial solution part is done, but the distortion part
+# is not.
+
+from libs.recipe_helper import RecipeHelper
+
+from process_wvlsol_v0 import extract_spectra, extract_spectra_multi
+from process_wvlsol_v0 import make_combined_image
+
+
+def fit_wvl_sol(helper, band, obsids):
+
+    caldb = helper.get_caldb()
+    master_obsid = obsids[0]
+    spec_json = caldb.load_item_from((band, master_obsid), "ONED_SPEC_JSON")
+
+    wvlsol_json = caldb.load_item_from((band, master_obsid),
+                                       "WVLSOL_V0_JSON")
+
+    assert len(wvlsol_json["orders"]) == len(spec_json["orders"])
+    orders = wvlsol_json["orders"]
+    wvlsol = wvlsol_json["wvl_sol"]
+    s = spec_json["specs"]
+
+    fitter = SkyFitter(helper.config, helper.refdate)
+    _ = fitter.fit(band, orders, wvlsol, s)
+    orders_w_solutions, wvl_sol, reidentified_lines_map, p, m = _
+    #_ = fitter.fit(band, orders, wvlsol["wvl_sol"], s["specs"])
+
+    save_wavelength_sol(helper, band, obsids,
+                        orders_w_solutions, wvl_sol, p)
+
+    save_qa(helper, band, obsids,
+            orders_w_solutions, reidentified_lines_map, p, m)
+
+
+def fit_wvl_sol2(helper, band, obsids):
+
+    caldb = helper.get_caldb()
+    master_obsid = obsids[0]
+    spec_json = caldb.load_item_from((band, master_obsid), "ONED_SPEC_JSON")
+
+    wvlsol_json = caldb.load_item_from((band, master_obsid),
+                                       "WVLSOL_V0_JSON")
+
+    assert len(wvlsol_json["orders"]) == len(spec_json["orders"])
+    orders = wvlsol_json["orders"]
+    wvlsol = wvlsol_json["wvl_sol"]
+    s = spec_json["specs"]
+
+    # load reference data
+    # find pixel positions from the given wavelength solution
+    # reidentify lines and find new pixel positions
+
+
+    fitter = SkyFitter(helper.config, helper.refdate)
+    _ = fitter.fit(band, orders, wvlsol, s)
+    orders_w_solutions, wvl_sol, reidentified_lines_map, p, m = _
+    #_ = fitter.fit(band, orders, wvlsol["wvl_sol"], s["specs"])
+
+    save_wavelength_sol(helper, band, obsids,
+                        orders_w_solutions, wvl_sol, p)
+
+    save_qa(helper, band, obsids,
+            orders_w_solutions, reidentified_lines_map, p, m)
+
+
+def process_band(utdate, recipe_name, band, obsids, config_name):
+
+    helper = RecipeHelper(config_name, utdate, recipe_name)
+
+    # STEP 1 :
+    ## make combined image
+
+    make_combined_image(helper, band, obsids, mode=None)
+
+    # Step 2
+
+    ## load simple-aperture (no order info; depends on
+
+    extract_spectra(helper, band, obsids)
+
+    extract_spectra_multi(helper, band, obsids)
+
+    fit_wvl_sol(helper, band, obsids)
+
+
+if 0:
+
+    # Step 3:
+
+    identify_lines(helper, band, obsids)
+
+    get_1d_wvlsol(helper, band, obsids)
+
+    save_1d_wvlsol(extractor,
+                   orders_w_solutions, wvl_sol, p)
+
+    save_qa(extractor, orders_w_solutions,
+            reidentified_lines_map, p, m)
+
+
+    save_figures(helper, band, obsids)
+
+    save_db(helper, band, obsids)
+
+
+
+if __name__ == "__main__":
+
+    utdate = "20140709"
+    obsids = [62, 63]
+
+    utdate = "20140525"
+    obsids = [29]
+
+    utdate = "20150525"
+    obsids = [52]
+
+
+    recipe_name = "SKY"
+
+
+    # utdate = "20150525"
+    # obsids = [32]
+
+    # recipe_name = "THAR"
+
+    band = "K"
+
+    #helper = RecipeHelper("../recipe.config", utdate)
+    config_name = "../recipe.config"
+
+    process_band(utdate, recipe_name, band, obsids, config_name)

recipes/test_identify_multiline.py
@@ -1,0 +1,105 @@
+import numpy as np
+import pandas as pd
+
+from libs.recipe_helper import RecipeHelper
+
+
+def process_band(utdate, recipe_name, band, obsids, config_name):
+
+    helper = RecipeHelper(config_name, utdate, recipe_name)
+
+    caldb = helper.get_caldb()
+
+    master_obsid = obsids[0]
+    basename = (band, master_obsid)
+
+    multi_spec = caldb.load_item_from((band, master_obsid),
+                                      "multi_spec_fits")
+
+    # just to retrieve order information
+    wvlsol_v0 = caldb.load_resource_for(basename, "wvlsol_v0")
+    orders = wvlsol_v0["orders"]
+    wvlsol = wvlsol_v0["wvl_sol"]
+
+    #
+    from collections import namedtuple
+    Spec = namedtuple("Spec", ["s_map", "wvl_map"])
+
+    keys = []
+    fitted_pixels_list = []
+
+    from libs.ref_lines_db import SkyLinesDB, HitranSkyLinesDB
+
+    ref_lines_db = SkyLinesDB(config=config_name)
+    ref_lines_db_hitrans = HitranSkyLinesDB(config=config_name)
+
+    for hdu in multi_spec:
+
+        slit_center = hdu.header["FSLIT_CN"]
+
+        spec = Spec(dict(zip(orders, hdu.data)),
+                    dict(zip(orders, wvlsol)))
+
+        fitted_pixels_list.append(ref_lines_db.identify(band, spec))
+        keys.append((slit_center, "OH"))
+
+        if band == "K":
+            fitted_pixels_hitran = ref_lines_db_hitrans.identify(band, spec)
+            fitted_pixels_list.append(fitted_pixels_hitran)
+            keys.append((slit_center, "Hitran"))
+
+    # concatenate collected list of fitted pixels.
+    fitted_pixels_master = pd.concat(fitted_pixels_list,
+                                     keys=keys,
+                                     names=["slit_center", "kind"],
+                                     axis=0)
+
+    if 0:
+        # storing multiindex seems broken. Enforce reindexing.
+        fitted_pixels_master.reset_index().to_json("test.json",
+                                                   orient="split")
+        fitted_pixels_master.reset_index().to_csv("test.csv")
+
+if 0:
+    filename = "test.json"
+    fitted_pixels_master0 = pd.read_json(filename, orient="split")
+    filename = "test.csv"
+    fitted_pixels_master0 = pd.read_csv(filename)
+    index = ["slit_center", "kind", "order", "wavelength"]
+    fitted_pixels_master = fitted_pixels_master0.set_index(index)
+
+    for do, group in fitted_pixels_master.groupby(level=0):
+        o = group.index.get_level_values("order")
+        #do = group.index.get_level_values("order")
+        x = group["pixels"]
+        #print len(group)
+
+        plot(x, o+do, ".")
+
+
+if __name__ == "__main__":
+
+    utdate = "20140709"
+    obsids = [62, 63]
+
+    utdate = "20140525"
+    obsids = [29]
+
+    utdate = "20150525"
+    obsids = [52]
+
+
+    recipe_name = "SKY"
+
+
+    # utdate = "20150525"
+    # obsids = [32]
+
+    # recipe_name = "THAR"
+
+    band = "K"
+
+    #helper = RecipeHelper("../recipe.config", utdate)
+    config_name = "../recipe.config"
+
+    process_band(utdate, recipe_name, band, obsids, config_name)

recipes/test_volume_fit.py
@@ -1,0 +1,154 @@
+import pandas as pd
+import numpy as np
+
+from numpy.linalg import lstsq
+
+from nd_poly import NdPolyNamed
+
+
+def get_center(key_list):
+    key_list = sorted(key_list)
+    n = len(key_list)
+    assert divmod(n, 2)[1] == 1
+    center_key = key_list[divmod(n, 2)[0]]
+    return center_key
+
+
+def append_offset(df):
+    """
+    input should be indexed with multiple values of 'slit_center'.
+    Columns of 'pixel0' and 'offsets' will be appended and returned.
+    """
+
+    grouped = df.groupby("slit_center")
+
+    slit_center0 = get_center(grouped.groups.keys())
+    rename_dict = {'pixels': 'pixels0'}
+    center = grouped.get_group(slit_center0).rename(columns=rename_dict)
+
+    pp = df.join(center["pixels0"])
+
+    pp["offsets"] = pp["pixels"] - pp["pixels0"]
+    pp_masked = pp[np.isfinite(pp["offsets"])]
+
+    df_offset = pp_masked.reset_index()
+
+    return df_offset
+
+
+def volume_poly_fit(points, scalar, orders, names):
+
+    p = NdPolyNamed(orders, names)  # order 2 for all dimension.
+
+    v = p.get_array(points)
+    v = np.array(v)
+
+    # errors are not properly handled for now.
+    s = lstsq(v.T, scalar)
+
+    return p, s
+
+
+def test_fit():
+    df = pd.read_json("test.json", orient="split")
+    index_names = ["kind", "order", "wavelength"]
+    df = df.set_index(index_names)[["slit_center", "pixels"]]
+
+    dd = append_offset(df)
+
+    names = ["pixel", "order", "slit"]
+    orders = [2, 2, 2]
+
+    points = dict(zip(names, [dd["pixels0"], dd["order"], dd["slit_center"]]))
+    scalar = dd["offsets"]
+
+    poly, params = volume_poly_fit(points, scalar, orders, names)
+
+    # save
+    out_df = poly.to_pandas(coeffs=params[0])
+    out_df = out_df.reset_index()
+
+    out_df.to_json("coeffs.json", orient="split")
+
+    # read
+    in_df = pd.read_json("coeffs.json", orient="split")
+    in_df = in_df.set_index(["pixel", "order", "slit"])
+
+    poly, coeffs = NdPolyNamed.from_pandas(in_df)
+
+
+def process_band_make_offset_map(utdate, recipe_name, band,
+                                 obsids, config_name):
+
+    from libs.recipe_helper import RecipeHelper
+    helper = RecipeHelper(config_name, utdate, recipe_name)
+
+    caldb = helper.get_caldb()
+
+    master_obsid = obsids[0]
+    basename = (band, master_obsid)
+
+    ordermap_fits = caldb.load_resource_for(basename,
+                                            ("sky", "ordermap_fits"))
+
+    slitposmap_fits = caldb.load_resource_for(basename,
+                                              ("sky", "slitposmap_fits"))
+
+    # slitoffset_fits = caldb.load_resource_for(basename,
+    #                                           ("sky", "slitoffset_fits"))
+
+    yy, xx = np.indices(ordermap_fits[0].data.shape)
+
+    msk = np.isfinite(ordermap_fits[0].data)
+    pixels, orders, slit_pos = (xx[msk], ordermap_fits[0].data[msk],
+                                slitposmap_fits[0].data[msk])
+
+    # load coeffs
+    # This needs to be fixed
+    names = ["pixel", "order", "slit"]
+
+    in_df = pd.read_json("coeffs.json", orient="split")
+    in_df = in_df.set_index(names)
+
+    poly, coeffs = NdPolyNamed.from_pandas(in_df)
+
+    values = dict(zip(names, [pixels, orders, slit_pos]))
+    offsets = poly.multiply(values, coeffs)
+
+    offset_map = np.empty(ordermap_fits[0].data.shape, dtype=np.float64)
+    offset_map.fill(np.nan)
+    offset_map[msk] = offsets  # dd["offsets"]
+
+
+
+
+if 0:
+    xy = np.indices([60, 2048]) / np.array([60., 1.])[:, np.newaxis, np.newaxis]
+
+    p93, params93 = p.freeze("order", 93, params)
+    offset93 = p93.multiply(dict(pixel=xy[1], slit=xy[0]), params93)
+
+    p72, params72 = p.freeze("order", 72, params)
+    offset72 = p93.multiply(dict(pixel=xy[1], slit=xy[0]), params72)
+
+
+if 0:
+    offsets_fit = p.multiply(values, params)
+
+    #plot(values["slit"], offsets, "x")
+    plot(values["slit"], offsets_fit - offsets, "o")
+
+
+if __name__ == "__main__":
+    utdate = "20150525"
+    obsids = [52]
+
+    recipe_name = "SKY"
+
+    band = "K"
+
+    #helper = RecipeHelper("../recipe.config", utdate)
+    config_name = "../recipe.config"
+
+    process_band_make_offset_map(utdate, recipe_name, band,
+                                 obsids, config_name)

commit 73168af58186f20fa7fc8ee3c1413e68b634c876

fix error in caldb introduced in 40592b8

libs/cal_db.py
@@ -211,18 +211,21 @@ class CalDB(object):
         try:
             db_name, item_desc = self.RESOURCE_DICT.get(resource_type,
                                                         resource_type)
-            db_name, item_desc = resource_type
         except ValueError as e:
             raise e  # it would be good if we can modify the message
 
-        return basename, item_desc
+        resource_basename = self.db_query_basename(db_name, band,
+                                                   master_obsid)
+
+        return resource_basename, item_desc
 
     def load_resource_for(self, basename, resource_type):
         """
         this load resource from the given master_obsid.
         """
 
-        resource_basename, item_desc = self.query_resource_for(basename, resource_type)
+        resource_basename, item_desc = self.query_resource_for(basename,
+                                                               resource_type)
 
         resource = self.load_item_from(resource_basename, item_desc)
 