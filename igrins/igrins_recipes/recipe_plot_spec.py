1a6b9df53b117c6d519e3aedfdde72916bf2b4db
Jae-Joon Lee <lee.j.joon@gmail.com>
2018-05-14 13:48:12 +0900

    update plot_spec recipe

--- igrins/igrins_recipes/recipe_plot_spec.py
+++ igrins/igrins_recipes/recipe_plot_spec.py
@@ -1,12 +1,14 @@
+import numpy as np
 import pandas as pd
 
-from ..pipeline.steps import Step
+from ..pipeline.steps import Step, ArghFactoryWithShort
 
 # from ..procedures.sky_spec import make_combined_image_sky
 
 # from ..utils.image_combine import image_median
 
 from ..igrins_libs.resource_helper_igrins import ResourceHelper
+from ..igrins_libs.oned_spec_helper import OnedSpecHelper
 
 
 def set_basename_postfix(obsset):
@@ -35,27 +37,82 @@ def _plot_source_spec(fig, tgt, objname=""):
         ax1a.set_title(objname)
 
 
-def _plot_div_a0v_spec(fig, tgt, obsset):
+from ..igrins_libs.a0v_obsid import get_group2, get_a0v_obsid
+
+def get_tgt_spec_cor(obsset, tgt, a0v, threshold_a0v, multiply_model_a0v):
+    tgt_spec_cor = []
+    #for s, t in zip(s_list, telluric_cor):
+    for s, t, t2 in zip(tgt.spec,
+                        a0v.spec,
+                        a0v.flattened):
+
+        st = s/t
+        msk = np.isfinite(t)
+        if np.any(msk):
+            #print np.percentile(t[np.isfinite(t)], 95), threshold_a0v
+            t0 = np.percentile(t[msk], 95)*threshold_a0v
+            st[t<t0] = np.nan
+
+            st[t2 < threshold_a0v] = np.nan
+
+        tgt_spec_cor.append(st)
+
+
+    if multiply_model_a0v:
+        # multiply by A0V model
+        d = obsset.rs.load_ref_data("VEGA_SPEC")
+        from ..procedures.a0v_spec import A0VSpec
+        a0v_model = A0VSpec(d)
+
+        a0v_interp1d = a0v_model.get_flux_interp1d(1.3, 2.5,
+                                                   flatten=True,
+                                                   smooth_pixel=32)
+        for wvl, s in zip(tgt.um,
+                          tgt_spec_cor):
+
+            aa = a0v_interp1d(wvl)
+            s *= aa
+
+
+    return tgt_spec_cor
+
+
+def _plot_div_a0v_spec(fig, tgt, obsset, a0v="GROUP2", a0v_obsid=None,
+                       threshold_a0v=0.1,
+                       objname="",
+                       multiply_model_a0v=False,
+                       html_output=False,
+                       a0v_basename_postfix=""):
     # FIXME: This is simple copy from old version.
 
-    if True:
+    a0v_obsid = get_a0v_obsid(obsset, a0v, a0v_obsid)
+    if a0v_obsid is None:
+        a0v_obsid_ = obsset.query_resource_basename("a0v")
+        a0v_obsid = obsset.rs.parse_basename(a0v_obsid_)
 
-        if (a0v_obsid is None) or (a0v_obsid == "1"):
-            A0V_basename = extractor.basenames["a0v"]
-        else:
-            A0V_basename = "SDC%s_%s_%04d" % (band, utdate, int(a0v_obsid))
-            print(A0V_basename)
+    a0v_obsset = type(obsset)(obsset.rs, "A0V_AB", [a0v_obsid], ["A"],
+                              basename_postfix=a0v_basename_postfix)
 
-        a0v = extractor.get_oned_spec_helper(A0V_basename,
-                                             basename_postfix=basename_postfix)
+    a0v = OnedSpecHelper(a0v_obsset)
 
-        tgt_spec_cor = get_tgt_spec_cor(tgt, a0v,
-                                        threshold_a0v,
-                                        multiply_model_a0v,
-                                        config)
+    # if True:
 
-    ax2a = fig2.add_subplot(211)
-    ax2b = fig2.add_subplot(212, sharex=ax2a)
+    #     if (a0v_obsid is None) or (a0v_obsid == "1"):
+    #         A0V_basename = extractor.basenames["a0v"]
+    #     else:
+    #         A0V_basename = "SDC%s_%s_%04d" % (band, utdate, int(a0v_obsid))
+    #         print(A0V_basename)
+
+    #     a0v = extractor.get_oned_spec_helper(A0V_basename,
+    #                                          basename_postfix=basename_postfix)
+    # config = obsset.rs.config
+
+    tgt_spec_cor = get_tgt_spec_cor(obsset, tgt, a0v,
+                                    threshold_a0v,
+                                    multiply_model_a0v)
+
+    ax2a = fig.add_subplot(211)
+    ax2b = fig.add_subplot(212, sharex=ax2a)
 
     #from ..libs.stddev_filter import window_stdev
 
@@ -123,7 +180,8 @@ def _save_to_html():
                       spec_js_name="jj_a0v.js")
 
 
-def plot_spec(obsset, interactive=False):
+def plot_spec(obsset, interactive=False,
+              multiply_model_a0v=False):
     recipe = obsset.recipe_name
     target_type, nodding_type = recipe.split("_")
 
@@ -134,7 +192,6 @@ def plot_spec(obsset, interactive=False):
     else:
         raise ValueError("Unknown recipe : %s" % recipe)
 
-    from ..igrins_libs.oned_spec_helper import OnedSpecHelper
     tgt = OnedSpecHelper(obsset)
 
     do_interactive_figure = interactive
@@ -151,11 +208,12 @@ def plot_spec(obsset, interactive=False):
 
     _plot_source_spec(fig1, tgt)
 
-    # if FIX_TELLURIC:
-    #     fig1 = Figure(figsize=(12, 6))
-    #     fig_list.append(fig1)
+    if FIX_TELLURIC:
+        fig1 = Figure(figsize=(12, 6))
+        fig_list.append(fig1)
 
-    #     _plot_div_a0v_spec(fig1, tgt, obsset)
+        _plot_div_a0v_spec(fig1, tgt, obsset,
+                           multiply_model_a0v=multiply_model_a0v)
 
     if fig_list:
         for fig in fig_list:
@@ -167,5 +225,7 @@ def plot_spec(obsset, interactive=False):
 
 
 steps = [Step("Set basename_postfix", set_basename_postfix),
-         Step("Plot spec", plot_spec, interactive=False),
+         Step("Plot spec", plot_spec,
+              interactive=ArghFactoryWithShort(False),
+              multiply_model_a0v=ArghFactoryWithShort(False)),
 ]
