dbc700a8f999470c79486dca356bbf7d2de02f7a
Jae-Joon Lee <lee.j.joon@gmail.com>
2019-07-25 13:41:08 +0900

    update recipe_func

--- igrins/pipeline/main_recipe.py
+++ igrins/pipeline/main_recipe.py
@@ -126,45 +126,30 @@ driver_args = [arg("-b", "--bands", default="HK", choices=["HK", "H", "K"]),
                arg("-d", "--debug", default=False)]
 
 
-def driver_func(command_name, steps, recipe_name_fnmatch, obsdate,
-                bands="HK", groups=None,
-                config_file=None, debug=False, verbose=None,
-                override_recipe_name=False,
-                # resume_from_context_file=None,
-                save_context_if="never",
-                context_name="context_{obsdate}_{recipe_name}_{groupname}{basename_postfix}_{context_id}.pickle",
-                # save_context_on_exception=False,
-                step_range=None,
-                save_io_log=False,
-                io_log_name="SDC{band}_{obsdate}_{groupname}{basename_postfix}.{command_name}.io",
-                **kwargs):
-
-    # FIXME : should check if 'resume_from_context_file' and 'step_range' are
-    # not set together.
-
+def driver_func_obsset(command_name, obsdate, steps, obsset_list,
+                       debug=False, verbose=None,
+                       # resume_from_context_file=None,
+                       save_context_if="never",
+                       context_name="context_{obsdate}_{recipe_name}_{groupname}{basename_postfix}_{context_id}.pickle",
+                       # save_context_on_exception=False,
+                       step_range=None,
+                       save_io_log=False,
+                       io_log_name="SDC{band}_{obsdate}_{groupname}{basename_postfix}.{command_name}.io",
+                       progress_mode=False,
+                       **kwargs):
     if step_range is not None:
         _se = [k for k in step_range.split(":")]
-        if len(_se)  == 1:
+        if len(_se) == 1:
             k = int(_se[0])
             _s, _e = k, k + 1
         elif len(_se) == 2:
             _s, _e= [int(k) if k.strip() else None for k in _se]
         else:
-            raise ValueError("incorrect step_range: {}".format(ste_range))
+            raise ValueError("incorrect step_range: {}".format(step_range))
 
         step_slice = slice(_s, _e)
     else:
         step_slice = slice(None, None)
-
-    if override_recipe_name:
-        if groups is None:
-            raise RuntimeError("override_recipe_name should specify groups.")
-
-        recipe_name_fnmatch = ["*"]
-
-    obsset_list = [obsset for obsset in iter_obsset(recipe_name_fnmatch,
-                                                    obsdate, config_file,
-                                                    bands, groups)]
 
     if save_context_if == "always":
         save_context = True
@@ -197,7 +182,7 @@ def driver_func(command_name, steps, recipe_name_fnmatch, obsdate,
 
         if step_slice.start:
 
-            context_id=step_slice.start
+            context_id = step_slice.start
             outname = context_name.format(obsdate=obsdate,
                                           context_id=context_id,
                                           **obsset_desc)
@@ -205,11 +190,12 @@ def driver_func(command_name, steps, recipe_name_fnmatch, obsdate,
 
         apply_steps(obsset, steps, step_slice=step_slice,
                     kwargs=kwargs,
-                    on_raise=on_raise)
+                    on_raise=on_raise,
+                    progress_mode=progress_mode)
 
         if (save_context or
             (step_slice.stop is not None and step_slice.stop < len(steps))):
-            context_id=step_slice.stop
+            context_id = step_slice.stop
 
             outname = context_name.format(obsdate=obsdate,
                                           context_id=context_id,
@@ -226,3 +212,44 @@ def driver_func(command_name, steps, recipe_name_fnmatch, obsdate,
                                              )
 
             obsset.rs.save_io_items(open(io_log_name, "w"))
+
+
+def driver_func(command_name, steps, recipe_name_fnmatch, obsdate,
+                bands="HK", groups=None, config_file=None,
+                override_recipe_name=False,
+                **kwargs):
+
+    # FIXME : should check if 'resume_from_context_file' and 'step_range' are
+    # not set together.
+
+    if override_recipe_name:
+        if groups is None:
+            raise RuntimeError("override_recipe_name should specify groups.")
+
+        recipe_name_fnmatch = ["*"]
+
+    obsset_list = [obsset for obsset in iter_obsset(recipe_name_fnmatch,
+                                                    obsdate, config_file,
+                                                    bands, groups)]
+
+    driver_func_obsset(command_name, obsdate, steps, obsset_list,
+                       **kwargs)
+
+
+if hasattr(driver_func, "__signature__"):
+    import inspect
+
+    def merge_signature(f1, f2):
+        s1 = inspect.signature(f1)
+        s2 = inspect.signature(f2)
+
+        filtered_ss = (list(s1.parameters.values())[:-1]
+                       + [_s for _s in s2.parameters.values()
+                          if ((_s.default is not inspect._empty) or
+                              (_s.kind is inspect.Parameter.VAR_KEYWORD))])
+
+        return inspect.Signature(filtered_ss)
+
+    driver_func.__signature__ = merge_signature(driver_func,
+                                                driver_func_obsset)
+
