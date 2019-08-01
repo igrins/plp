012f87ffef59efad8860283e52f14eaaf3cf0ea8
Jae-Joon Lee <lee.j.joon@gmail.com>
2019-08-01 18:29:07 +0900

    update refined_argmax.py

--- igrins/utils/refined_argmax.py
+++ igrins/utils/refined_argmax.py
@@ -6,6 +6,33 @@ to find 2nd order polynomial and its peak.
 import numpy as np
 
 
+def find_peak_poly2d(kk):
+    """
+    find the peak location and peak value form
+
+    y = ax^2 + bc + c
+    where kk is a 3 values (or list of 3 values) given at [-1, 0, 1]
+
+    returns x at the peak and peak it self
+    """
+
+    # find a,b,c (from v = a y^2 + b y + c) and do y@max = -b/2a
+    c = kk[1]
+    a = .5 * (kk[2] + kk[0] - 2 * c)
+    b = .5 * (kk[2] - kk[0])
+
+    x = -b / a / 2.
+
+    peak = -1. * b * b / (4. * a) + c
+
+    return x, peak
+
+
+def test_peak():
+    x, peak = find_peak_poly2d([-1, 1, 1])
+    assert (x, peak) == (0.5, 1.25)
+
+
 def refined_argmax(d, m):
     ym = np.ma.array(d, mask=~m).argmax(axis=0)
 
@@ -22,8 +49,13 @@ def refined_argmax(d, m):
     kk[:, bad_columns] = np.nan
 
     # find a,b,c (from v = a y^2 + b y + c) and do y@max = -b/2a
-    a = .5 * (kk[2] + kk[0] - 2 * kk[1])
+    c = kk[1]
+    a = .5 * (kk[2] + kk[0] - 2 * c)
     b = .5 * (kk[2] - kk[0])
+
     dy = -b / a / 2.
 
-    return ym, dy
+    peak = 3. * b * b / (4. * a * a) + c
+    dy, peak = find_peak_poly2d(kk)
+
+    return ym, dy, peak
