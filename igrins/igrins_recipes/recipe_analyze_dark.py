6721706819198aea171735c3eedb90ccc0ae37a7
Jae-Joon Lee <lee.j.joon@gmail.com>
2019-08-18 15:28:20 +0900

    change default use_bias_mask to True

--- igrins/igrins_recipes/recipe_analyze_dark.py
+++ igrins/igrins_recipes/recipe_analyze_dark.py
@@ -8,7 +8,7 @@ from ..procedures.procedure_dark import (make_guard_n_bg_subtracted_images,
 
 
 steps = [Step("Make RO pattern-subtracted images",
-              make_guard_n_bg_subtracted_images, use_bias_mask=False),
+              make_guard_n_bg_subtracted_images, use_bias_mask=True),
          Step("Esimate amp-wise noise",
               estimate_amp_wise_noise),
          Step("Printing out the stat summary",
