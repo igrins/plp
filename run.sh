9f14eb2100573564724426d1c8dbe46994fbc5a4
Kyle Kaplan <kyle.kaplan@austin.utexas.edu>
2024-04-01 12:24:53 -0500

    Clean up run.sh script.

--- run.sh
+++ run.sh
@@ -2,17 +2,13 @@
 #Simple script to fully run IGRINS PLP on any UTDATE
 echo "Enter UTDATE:" #Prompt user to enter date
 read UTDATE #Get date from user
+
 #Clean up previous files, if any
 rm -r outdata/$UTDATE
 rm -r calib/secondary/$UTDATE
 rm -r calib/primary/$UTDATE
+
 #Run PLP
-
-
-# python ./igr_pipe.py clean-pattern-setup $UTDATE #Setup files needed for flexure correction
-
-
-
 python igr_pipe.py flat $UTDATE #Process flat
 python ./igr_pipe.py register-sky $UTDATE #First pass at wavelength solution using sky frame (incase THAR lamps don't exist)
 python ./igr_pipe.py flexure-setup $UTDATE #Setup files needed for flexure correction
@@ -20,9 +16,9 @@ python ./igr_pipe.py flexure-setup $UTDATE #Setup files needed for flexure corre
 python ./igr_pipe.py wvlsol-sky $UTDATE #Second pass at wavelength solution using OH sky emission
 python ./igr_pipe.py a0v-ab $UTDATE --correct-flexure --mask-cosmics #Reduce A0V standard star data
 python ./igr_pipe.py a0v-onoff $UTDATE --correct-flexure --mask-cosmics
-python ./igr_pipe.py tell-wvsol $UTDATE #Use telluric absorption in A0V std star to fine tune wavelength solution
-python ./igr_pipe.py stellar-ab $UTDATE --correct-flexure --mask-cosmics #Reduce stellar sources nod on slit
-python ./igr_pipe.py stellar-onoff $UTDATE --correct-flexure --mask-cosmics #Reduce stellar sources nod off slit
+#python ./igr_pipe.py tell-wvsol $UTDATE #Use telluric absorption in A0V std star to fine tune wavelength solution
+python ./igr_pipe.py stellar-ab $UTDATE --correct-flexure --height-2dspec=100 --mask-cosmics #Reduce stellar sources nod on slit
+python ./igr_pipe.py stellar-onoff $UTDATE --correct-flexure --height-2dspec=100 --mask-cosmics #Reduce stellar sources nod off slit
 python ./igr_pipe.py extended-ab $UTDATE --correct-flexure --height-2dspec=100 --mask-cosmics #Reduce extended sources nod on slit
 python ./igr_pipe.py extended-onoff $UTDATE --correct-flexure --height-2dspec=100 --mask-cosmics #Reduce extended sources not off slit
 python ./igr_pipe.py divide-a0v $UTDATE #Reduce stellar sources nod off slit
