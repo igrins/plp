e149153fb28367ac6f8fb0cc8e39fa2ac82d274f
Hyun-Jeong Kim <hjkim@kasi.re.kr>
2024-05-13 19:53:47 +0900

    apply file_changes

--- igr_pipe.py
+++ igr_pipe.py
@@ -1,1 +1,24 @@
-igrins/igr_pipe.py
+#! /usr/bin/env python
+
+import sys
+
+import igrins.igrins_libs.logger as logger
+from igrins.igrins_recipes.arghed_recipes import get_recipe_list
+from igrins.pipeline.argh_helper import argh
+
+def main():
+    parser = argh.ArghParser()
+    recipe_list = get_recipe_list()
+    parser.add_commands(recipe_list)
+
+    import numpy
+    numpy.seterr(all="ignore")
+    argv = sys.argv[1:]
+    if "--debug" in argv:
+        argv.remove("--debug")
+        logger.set_level("debug")
+
+    argh.dispatch(parser, argv=argv)
+
+if __name__ == '__main__':
+    main()
