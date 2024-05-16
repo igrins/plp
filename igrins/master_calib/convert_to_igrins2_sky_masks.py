c368462fab8223f701da96f67fdeab875c9b9065
Kyle Kaplan <kyle.kaplan@austin.utexas.edu>
2024-05-15 19:32:16 -0500

    Modifying sky masks a little.

--- igrins/master_calib/convert_to_igrins2_sky_masks.py
+++ igrins/master_calib/convert_to_igrins2_sky_masks.py
@@ -7,19 +7,19 @@ from astropy.io import fits
 #Convert H-band
 
 d = fits.getdata('H-band_sky_mask.fits')
-d2 = binary_dilation(np.roll(np.roll(d, 30, axis=0), -140, axis=1), iterations=10)*1
+d2 = np.roll(np.roll(d, 30, axis=0), -140, axis=1)*1
 fits.writeto('H-band_sky_mask_igrins2.fits', d2, overwrite=True)
 
 d = fits.getdata('H-band_limited_sky_mask.fits')
-d2 = binary_dilation(np.roll(np.roll(d, 30, axis=0), -140, axis=1), iterations=10)*1
+d2 = np.roll(np.roll(d, 30, axis=0), -140, axis=1)*1
 fits.writeto('H-band_limited_sky_mask_igrins2.fits', d2, overwrite=True)
 
 #Convert K-band
 
 d = fits.getdata('K-band_sky_mask.fits')
-d2 = binary_dilation(np.roll(np.roll(d, 85, axis=0), -40, axis=1), iterations=5)*1
+d2 = np.roll(np.roll(d, 85, axis=0), -40, axis=1)*1
 fits.writeto('K-band_sky_mask_igrins2.fits', d2, overwrite=True)
 
 d = fits.getdata('K-band_limited_sky_mask.fits')
-d2 = binary_dilation(np.roll(np.roll(d, 85, axis=0), -40, axis=1), iterations=5)*1
+d2 = np.roll(np.roll(d, 85, axis=0), -40, axis=1)*1
 fits.writeto('K-band_limited_sky_mask_igrins2.fits', d2, overwrite=True)
